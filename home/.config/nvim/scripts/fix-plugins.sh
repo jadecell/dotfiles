d1e9126df4808e3565c73f6973fbc98d2c18f983
Jackson McCrory <jackson@mccrory.xyz>
2021-04-10 22:44:22 -0700

    Fixed up some scripts for neovim

--- home/.config/nvim/scripts/fix-plugins.sh
+++ home/.config/nvim/scripts/fix-plugins.sh
@@ -2,14 +2,14 @@
 set -eu
 
 if [ ! command -v npm ]; then
-    sudo emerge npm
+    sudo emerge --noreplace nodejs
 fi
 
 cd ~/.local/share/nvim/site/pack/packer/start/bracey.vim/ || exit 1
 npm install --prefix server
 
 if [ ! command -v yarn ]; then
-    sudo emerge yarn
+    sudo emerge --noreplace yarn
 fi
 
 cd ~/.local/share/nvim/site/pack/packer/start/vim-prettier || exit 1
--- home/.config/nvim/scripts/install-language-servers.sh
+++ home/.config/nvim/scripts/install-language-servers.sh
@@ -2,12 +2,12 @@
 set -eu
 
 # Checks for npm and installs it if it is not present
-# if [ ! command -v npm ] > /dev/null 2>&1; then
-#     sudo pacman --noconfirm --needed -S npm nodejs
-# fi
-#
-# Installs all the language servers with npm
-# sudo npm install -g bash-language-server vscode-css-languageserver-bin vscode-html-languageserver-bin typescript typescript-language-server vscode-json-languageserver pyright vim-language-server yaml-language-server
+if [ ! command -v npm ] > /dev/null 2>&1; then
+    sudo emerge --noreplace nodejs
+fi
+
+# # Installs all the language servers with npm
+sudo npm install -g bash-language-server vscode-css-languageserver-bin vscode-html-languageserver-bin typescript typescript-language-server vscode-json-languageserver pyright vim-language-server yaml-language-server
 
 cd ~/.config/nvim/ls || exit 1
 rm -rf lua-language-server
