4ae8dfc9cccc99ba66005ed99c641c89a5299d68
Jackson McCrory <jackson@mccrory.xyz>
2021-04-10 23:44:19 -0700

    Added neovim-head install script

---
+++ home/.config/nvim/scripts/install-neovim-head.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -eu
+
+CPUTHREADS="$(grep -c processor /proc/cpuinfo)"
+sudo mkdir -p /usr/local/bin/
+
+cd ~
+sudo rm -rf neovim
+git clone https://github.com/neovim/neovim
+cd neovim
+sudo make CMAKE_BUILD_TYPE=Release -j$CPUTHREADS install
+sudo cp build/bin/neovim /usr/local/bin/
+cd ~
+sudo rm -rf neovim
