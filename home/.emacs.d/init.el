a172bd2150c45fedf449536f08d178d3b805660e
Jackson McCrory <jackson@mccrory.xyz>
2021-04-18 00:19:16 -0700

    Added a ton of stuff to emacs

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -9,8 +9,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" default))
+ '(git-gutter:added-sign "++")
+ '(git-gutter:deleted-sign "--")
+ '(git-gutter:modified-sign "~~")
+ '(git-gutter:update-interval 1)
  '(package-selected-packages
-   '(git-gutter flycheck emmet-mode company-web yasnippet-snippets yasnippet ini-mode yaml-mode impatient-mode auto-rename-tag web-mode typescript-mode lsp-ui lsp-mode rainbow-delimiters autopair evil-nerd-commenter visual-fill-column all-the-icons-dired which-key-posframe vterm use-package projectile peep-dired org-bullets magit-todos ivy-rich ivy-posframe haskell-mode general evil-collection eshell-syntax-highlighting doom-themes doom-modeline dired-open dashboard counsel)))
+   '(shfmt lsp-pyright json-mode prettier git-gutter flycheck emmet-mode company-web yasnippet-snippets yasnippet ini-mode yaml-mode impatient-mode auto-rename-tag web-mode typescript-mode lsp-ui lsp-mode rainbow-delimiters autopair evil-nerd-commenter visual-fill-column all-the-icons-dired which-key-posframe vterm use-package projectile peep-dired org-bullets magit-todos ivy-rich ivy-posframe haskell-mode general evil-collection eshell-syntax-highlighting doom-themes doom-modeline dired-open dashboard counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
