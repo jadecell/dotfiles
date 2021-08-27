9ab63a9cea54cd8d03f27aaff5db9186bad14c03
Jackson McCrory <jackson@mccrory.xyz>
2021-08-27 16:40:05 -0700

    5.13.13

--- home/.config/nvim/scripts/fix-plugins.sh
+++ home/.config/nvim/scripts/fix-plugins.sh
@@ -5,14 +5,14 @@ if [ ! "$(command -v npm)" ]; then
     sudo pacman --needed --noconfirm -S npm nodejs
 fi
 
-cd ~/.local/share/nvim/site/pack/packer/start/bracey.vim/ || exit 1
+cd ~/.local/share/lunarvim/site/pack/packer/start/bracey.vim/ || exit 1
 npm install --prefix server
 
 if [ ! "$(command -v yarn)" ]; then
     sudo pacman --needed --noconfirm -S yarn
 fi
 
-cd ~/.local/share/nvim/site/pack/packer/start/vim-prettier || exit 1
+cd ~/.local/share/lunarvim/site/pack/packer/start/vim-prettier || exit 1
 yarn install
 
 echo "Finished!"
